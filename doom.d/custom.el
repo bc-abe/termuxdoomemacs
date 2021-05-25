9460bf2b88b2f2b1b6bb7ae1f4261012b60a6a1b
bc-abe <abraham.peters@outlook.com>
2021-05-25 08:12:02 -0600

    Add files via upload

---
+++ doom.d/custom.el
@@ -0,0 +1,9 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files nil)
+ '(org-roam-bibtex-mode t)
+ '(package-selected-packages
+   '(ox-pandoc citeproc-org org-ref all-the-icons slack org-variable-pitch el-get)))
